be8b7380b0a7c27215127675752694de2a3965b9
Luis Felipe <felipelfoliveira@gmail.com>
2020-09-28 20:31:07 -0300

    Criação de métodos base para utilização da API integrada ao AWS S3

--- main_api.py
+++ main_api.py
@@ -1,29 +1,40 @@
-import uuid
-
 import boto3
 
-s3_client = boto3.resource('s3')
+s3_client = boto3.client('s3')
 
 
-def create_bucket_name(bucket_prefix):
-    return ''.join([bucket_prefix, str(uuid.uuid4())])
+# def generate_random_filename(file_name):
+#     random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
+#     return random_file_name
 
 
-def create_bucket(bucket_prefix, s3_connection):
-    session = boto3.session.Session()
-    current_region = session.region_name
-    bucket_name = create_bucket_name(bucket_prefix)
-    bucket_response = s3_connection.create_bucket(
-        Bucket=bucket_name,
-        CreateBucketConfiguration={
-            'LocationConstraint': current_region
-        }
-    )
-
-    print(bucket_name, current_region)
-    return bucket_name, bucket_response
+def upload_file(bucket_name, file_name, file_content):
+    s3_client.put_object(Bucket=bucket_name, Key=file_name, Body=file_content)
 
 
-def generate_random_filename(file_name):
-    random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
-    return random_file_name
+def download_file(bucket_name, file_key, file_name):
+    with open('/tmp/%s' % file_name, 'wb') as data:
+        s3_client.download_fileobj(bucket_name, file_key, data)
+
+    data.close()
+
+
+def list_objects(bucket_name):
+    objects_list = s3_client.list_objects(Bucket=bucket_name)['Contents']
+
+    for item in objects_list:
+        print("Chave: {}".format(item['Key']))
+
+
+def check_object(bucket_name, file_key):
+    objects_list = s3_client.list_objects(Bucket=bucket_name)['Contents']
+
+    for item in objects_list:
+        if item['Key'] == file_key:
+            print("Item encontrado!")
+
+
+def get_object_info(bucket_name, file_key):
+    object_info = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+
+    print(object_info)
