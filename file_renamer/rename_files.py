09d0f7b6709eb55b008e5dd3db12887d9e932490
Luis Felipe <felipelfoliveira@gmail.com>
2020-09-30 22:33:28 -0300

    Criação de métodos base para utilização da API integrada ao AWS S3

--- file_renamer/rename_files.py
+++ file_renamer/rename_files.py
@@ -1,29 +1,24 @@
+import json
 import os
 
 
-def obter_nome_antigo(filename):
+def obter_nome_antigo(filename, old_filenames):
     nome_antigo_padrao = "padrão"
 
-    dict_itens_old = {"digimon_2020": "digimon_adventure_2020_ep_", "maou_gakuin": "maou_gakuin_no_futekigousha_"}
-
-    for item in dict_itens_old:
-        if dict_itens_old[item][0:10] == filename[0:10]:
-            print("[Obtendo nome do item...]")
-            return dict_itens_old[item]
+    for item in old_filenames:
+        if old_filenames[item][0:10] == filename[0:10]:
+            return old_filenames[item]
 
     return nome_antigo_padrao
 
 
-def obter_nome_novo(old_filename):
-    dict_itens_new = {"digimon_adventure_2020_ep_": "Digimon Adventure (2020) - ",
-                      "maou_gakuin_no_futekigousha_": "Maou Gakuin no Futekigousha - "}
-
-    for item in dict_itens_new:
+def obter_nome_novo(old_filename, new_filenames):
+    for item in new_filenames:
         if item == old_filename:
-            return dict_itens_new[item]
+            return new_filenames[item]
 
 
-def renomear_arquivos(diretorio):
+def renomear_arquivos(diretorio, old_filenames, new_filenames):
     qtd_arquivos = 0
 
     for file in os.listdir(diretorio):
@@ -31,21 +26,20 @@ def renomear_arquivos(diretorio):
             filename = os.path.splitext(file)[0]
             extension = os.path.splitext(file)[1]
 
-            nome_antigo = obter_nome_antigo(filename)
-            nome_novo = obter_nome_novo(nome_antigo)
+            nome_antigo = obter_nome_antigo(filename, old_filenames)
+            nome_novo = obter_nome_novo(nome_antigo, new_filenames)
 
             if filename[0:10] == nome_antigo[0:10]:
 
                 old_filename_ep = filename.split('_')[-1]
 
-                if int(old_filename_ep) < 10:
+                if int(old_filename_ep) < 10 and "0" not in old_filename_ep:
                     old_filename_ep = "0" + old_filename_ep
 
-                full_new_name = nome_novo + old_filename_ep + extension
+                full_name = nome_novo + old_filename_ep + extension
 
-                os.rename(os.path.join(diretorio, file), os.path.join(diretorio, full_new_name))
+                os.rename(os.path.join(diretorio, file), os.path.join(diretorio, full_name))
 
-                print("\n[O arquivo %s foi renomeado para %s]" % (filename, filename.replace(filename, full_new_name)))
                 qtd_arquivos += 1
 
-    print("\n[Execução concluída: %i arquivos renomeados]" % qtd_arquivos)
+    return json.dumps({"fim_execucao": "Execução concluída: %i arquivos renomeados" % qtd_arquivos})
--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-import os
+from flask import Flask, make_response, request
 
-from file_backup.backup_manager import inserir_arquivo, inserir_arquivo_lote
 from file_renamer.rename_files import renomear_arquivos
 
-
-def main():
-    print("--------------------- File Manager v1.0 --------------------- \n")
-    diretorio = str(input("-> Diretório de execução: "))
-    os.system("cls")
-    print("[1] - Renomear arquivos em lote por diretório\n")
-    print("[2] - Inserir arquivos em lote\n")
-    print("[3] - Inserir arquivo individual\n")
-    opcao = int(input("-> "))
-    os.system("cls")
-
-    if opcao == 1:
-        print("- O diretório será o mesmo inserido anteriormente?\n")
-        print("[1] - Sim\n")
-        print("[2] - Não\n")
-        diretorio_padrao = int(input("-> "))
-        os.system("cls")
-
-        if diretorio_padrao == 1:
-            renomear_arquivos(diretorio)
-        elif diretorio_padrao == 2:
-            novo_diretorio = str(input("-> Diretório de execução: "))
-            renomear_arquivos(novo_diretorio)
-    elif opcao == 2:
-        inserir_arquivo_lote(diretorio)
-    elif opcao == 3:
-        caminho_arquivo = str(input("-> Caminho do arquivo: "))
-        inserir_arquivo(caminho_arquivo)
+app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    main()
+@app.route('/api/local-management/files/rename', methods=['GET'])
+def local_file_rename():
+    headers = {"Content-Type": "application/json"}
+
+    req_content = request.json
+
+    return make_response(
+        renomear_arquivos(
+            req_content['directory'],
+            req_content['old_filenames'],
+            req_content['new_filenames']
+        ),
+        200,
+        headers
+    )
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8080')
